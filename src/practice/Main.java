6cb12995e103ad1b6e371da244ff7aacc9c27af7
vevdokimenko <evdokimenko@hotmail.com>
2021-11-20 12:55:28 +0200

    Practice

---
+++ src/practice/Main.java
@@ -0,0 +1,51 @@
+package practice;
+
+/*
+* Создать массив на 1000 рандомных чисел. После чего запускаются две потока.
+* Первый поток находит максимум в массиве. Второй поток находит минимум в массиве.
+* Результаты вычислений возвращаются в метод main.
+* */
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10000);
+            System.out.println(array[i]);
+        }
+
+        MaxThread maxThread = new MaxThread(array);
+        maxThread.start();
+
+        MinThread minThread = new MinThread(array);
+        minThread.start();
+    }
+}
+
+class MaxThread extends Thread {
+    int[] arr;
+
+    public MaxThread(int[] arr) {
+        this.arr = arr;
+    }
+
+    @Override
+    public void run() {
+        int max = Arrays.stream(arr).max().getAsInt();
+        System.out.println("Max: " + max);
+    }
+}
+
+class MinThread extends MaxThread {
+    public MinThread(int[] arr) {
+        super(arr);
+    }
+
+    @Override
+    public void run() {
+        int min = Arrays.stream(super.arr).min().getAsInt();
+        System.out.println("Min: " + min);
+    }
+}
